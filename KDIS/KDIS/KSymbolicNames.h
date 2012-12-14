5165f06f3768560b41c67419438c6a28a6366275
karljj1 <karljj1@yahoo.com>
2012-12-14 16:46:46 +0000

    Started updating the KSymbolicNames.h file from draft 14.

--- KDIS/KDIS/KSymbolicNames.h
+++ KDIS/KDIS/KSymbolicNames.h
@@ -21,22 +21,49 @@ http://p.sf.net/kdis/UserGuide
 /********************************************************************
     KSymbolicNames
     created:    20/9/2008
+	updated:    14/12/2012
     author:     Karl Jones
 
     purpose:    DIS Symbolic names used to represent DIS numeric values
 *********************************************************************/
 
+#define AGG_RESPONSE                                10                                  // s
+#define ALL_AGGREGATES                              0xffff
 #define ALL_APPLIC                                  0xffff
+#define ALL_BEAMS                                   0xffff
+#define ALL_EMITTERS                                0xffff
 #define ALL_ENTITIES                                0xffff
+#define ALL_OBJECTS                                 0xffff
 #define ALL_SITES                                   0xffff
-#define COLLISION_THRSH_DFLT                        0.1
+#define COLLISION_ELASTIC_TIMEOUT                   5                                   // s
+#define COLLISION_THRSH                             0.1f                                // m/s
+#define DE_ENERGY_THRSH                             0.01f 
+#define DE_PRECISION_AIMING_THRSH                   0.5f                                // m
+#define DRA_ORIENT_THRSH_DFLT                       3                                   // degs
+#define DRA_POS_THRSH_DFLT                          1                                   // m
 #define D_SPOT_NO_ENTITY                            NO_SITE , NO_APPLIC , NO_ENTITY
-#define DRA_ORIENT_THRSH_DFLT                       3
-#define DRA_POS_THRSH_DFLT                          1
-#define EE_EL_THRSH_DFLT                            1
+#define EE_AD_PULRAT_THRSH                          0.017f                              // rads/s
+#define EE_AD_PULACC_THRSH                          0.017f                              // rads/s^2
+#define EE_AZ_THRSH                                 1                                   // degs
+#define EE_EL_THRSH                                 1                                   // degs
+#define EE_ERP_THRSH                                1                                   // dB
+#define EE_FREQ_THRSH                               1                                   // Hz
+#define EE_FRNG_THRSH                               1                                   // Hz
+#define EE_FT_VEL_THRSH                             1                                   // m/s
+#define EE_FT_ACC_THRSH                             1                                   // m/s^2
+#define EE_FT_MWD_THRSH                             10000                               //  m
+#define EE_FT_KT_THRSH                              10                                  // s
+#define EE_FT_ESP_THRSH                             10                                  // m
+#define EE_HIGH_DENSITY_THRSH                       10                                  // entities/beam
+#define EE_PRF_THRSH                                1                                   // Hz
+#define EE_PW_THRSH                                 1                                   // µs
 #define ENTITY_ID_UNKNOWN                           NO_SITE , NO_APPLIC , NO_ENTITY
+
+// TODO: You are here
+
+
 #define HRT_BEAT_MOVE_TIMER                         2
-#define HRT_BEAT_MPLIER                             2.4
+#define HRT_BEAT_MPLIER                             2.4f
 #define HRT_BEAT_TIMER                              5
 #define MUNITION_NOT_TRACKED                        NO_SITE , NO_APPLIC , NO_ENTITY
 #define NO_APPLIC                                   0
