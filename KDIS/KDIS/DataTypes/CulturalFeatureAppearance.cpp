3968c489b78b618ac38dc7f3be0265be4121d329
Dale Marchand <Marchand.Dale@idsi.com>
2017-06-16 14:15:35 -0700

    82 CulturalFeatureAppearance incorrectly supports Engine Smoke bit
    Modified the struct to eliminate bit-6 tracking.  Changed the get/set
    methods to get/is and use a KBOOL parameter.  Updated the GetAsString()
    method.

--- KDIS/KDIS/DataTypes/CulturalFeatureAppearance.cpp
+++ KDIS/KDIS/DataTypes/CulturalFeatureAppearance.cpp
@@ -39,168 +39,168 @@ using namespace ENUMS;
 
 void CulturalFeatureAppearance::SetEntityDamage( EntityDamage ED )
 {
-    m_Damage = ED;
+  m_Damage = ED;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 EntityDamage CulturalFeatureAppearance::GetEntityDamage() const
 {
-    return ( EntityDamage )m_Damage;
+  return ( EntityDamage )m_Damage;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-void CulturalFeatureAppearance::SetEntitySmoke( EntitySmoke ES )
+void CulturalFeatureAppearance::SetEntitySmokeEmanating( KBOOL ESE )
 {
-    m_Smoke = ES;
+  m_Smoke = ESE;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-EntitySmoke CulturalFeatureAppearance::GetEntitySmoke() const
+KBOOL CulturalFeatureAppearance::IsEntitySmokeEmanating() const
 {
-    return ( EntitySmoke )m_Smoke;
+  return static_cast<KBOOL>( m_Smoke );
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityFlamingEffect( KBOOL EFE )
 {
-    m_FlamingEffectField = EFE;
+  m_FlamingEffectField = EFE;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityFlaming() const
 {
-    return m_FlamingEffectField;
+  return m_FlamingEffectField;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityFrozenStatus( KBOOL EFS )
 {
-    m_FrozenStatus = EFS;
+  m_FrozenStatus = EFS;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityFrozen() const
 {
-    return m_FrozenStatus;
+  return m_FrozenStatus;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetInternalHeatStatusOn( KBOOL IHS )
 {
-	m_InternalHeatStatus = IHS;
+  m_InternalHeatStatus = IHS;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsInternalHeatOn() const
 {
-	return m_InternalHeatStatus;
+  return m_InternalHeatStatus;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityStateActive( KBOOL ES )
 {
-    m_State = !ES;
+  m_State = !ES;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityStateActive() const
 {
-    return !m_State;
+  return !m_State;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityExteriorLights( KBOOL EL )
 {
-	m_ExteriorLights = EL;
+  m_ExteriorLights = EL;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityExteriorLightsOn() const
 {
-	return m_ExteriorLights;
+  return m_ExteriorLights;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityInteriorLights( KBOOL IL )
 {
-	m_InteriorLights = IL;
+  m_InteriorLights = IL;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityInteriorLightsOn() const
 {
-	return m_InteriorLights;
+  return m_InteriorLights;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 void CulturalFeatureAppearance::SetEntityMaskedCloaked( KBOOL MC )
 {
-	m_MaskedCloaked = MC;
+  m_MaskedCloaked = MC;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::IsEntityMaskedCloaked() const
 {
-	return m_MaskedCloaked;
+  return m_MaskedCloaked;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KString CulturalFeatureAppearance::GetAsString() const
 {
-    KStringStream ss;
+  KStringStream ss;
 
-    ss << "Cultural Feature Appearance:"
-       << "\n\tDamage:                 " << GetEnumAsStringEntityDamage( m_Damage )
-       << "\n\tSmoke:                  " << GetEnumAsStringEntitySmoke( m_Smoke ) 
-       << "\n\tFlaming Effect:         " << m_FlamingEffectField		
-       << "\n\tFrozen Status:          " << m_FrozenStatus
-	   << "\n\tInternal Heat:          " << m_InternalHeatStatus
-       << "\n\tState:                  " << m_State
-	   << "\n\tExterior Lights:        " << m_InteriorLights
-	   << "\n\tInterior Lights:        " << m_InteriorLights
-	   << "\n\tMasked/Cloaked:         " << m_MaskedCloaked
-       << "\n";
+  ss << "Cultural Feature Appearance:"
+     << "\n\tDamage:                 " << GetEnumAsStringEntityDamage( m_Damage )
+     << "\n\tSmoke Emanating:        " << m_Smoke
+     << "\n\tFlaming Effect:         " << m_FlamingEffectField
+     << "\n\tFrozen Status:          " << m_FrozenStatus
+     << "\n\tInternal Heat:          " << m_InternalHeatStatus
+     << "\n\tState:                  " << m_State
+     << "\n\tExterior Lights:        " << m_InteriorLights
+     << "\n\tInterior Lights:        " << m_InteriorLights
+     << "\n\tMasked/Cloaked:         " << m_MaskedCloaked
+     << "\n";
 
-    return ss.str();
+  return ss.str();
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::operator == ( const CulturalFeatureAppearance & Value ) const
 {
-	// Lets do a single comparison instead of checking every field. 
-	// This struct is basically a KUINT32 so lets cast it to one and compare.
+  // Lets do a single comparison instead of checking every field.
+  // This struct is basically a KUINT32 so lets cast it to one and compare.
 
-	KUINT32 a = *( KUINT32 * )this;
-	KUINT32 b = *( KUINT32 * )&Value;
+  KUINT32 a = *( KUINT32 * )this;
+  KUINT32 b = *( KUINT32 * )&Value;
 
-    if( a != b )return false;
-    return true;
+  if( a != b )return false;
+  return true;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 KBOOL CulturalFeatureAppearance::operator != ( const CulturalFeatureAppearance & Value ) const
 {
-    return !( *this == Value );
+  return !( *this == Value );
 }
 
 //////////////////////////////////////////////////////////////////////////
--- KDIS/KDIS/DataTypes/CulturalFeatureAppearance.h
+++ KDIS/KDIS/DataTypes/CulturalFeatureAppearance.h
@@ -53,117 +53,117 @@ namespace DATA_TYPE {
 struct KDIS_EXPORT CulturalFeatureAppearance
 {
 protected:	
-	                                     // Bits 
-    KUINT32 m_Unused1               : 3; // 0-2   
-    KUINT32 m_Damage                : 2; // 3-4
-    KUINT32 m_Smoke                 : 2; // 5-6
-    KUINT32 m_Unused2               : 8; // 7-14    
-    KUINT32 m_FlamingEffectField    : 1; // 15
-    KUINT32 m_Unused3               : 5; // 16-20    
-    KUINT32 m_FrozenStatus          : 1; // 21
-    KUINT32 m_InternalHeatStatus    : 1; // 22
-    KUINT32 m_State                 : 1; // 23 
-    KUINT32 m_Unused4               : 4; // 24-27    
-	KUINT32 m_ExteriorLights        : 1; // 28
-	KUINT32 m_InteriorLights        : 1; // 29
-	KUINT32 m_Unused5               : 1; // 30
-	KUINT32 m_MaskedCloaked         : 1; // 31
+                                       // Bits
+  KUINT32 m_Unused1               : 3; // 0-2
+  KUINT32 m_Damage                : 2; // 3-4
+  KUINT32 m_Smoke                 : 1; // 5
+  KUINT32 m_Unused2               : 9; // 6-14
+  KUINT32 m_FlamingEffectField    : 1; // 15
+  KUINT32 m_Unused3               : 5; // 16-20
+  KUINT32 m_FrozenStatus          : 1; // 21
+  KUINT32 m_InternalHeatStatus    : 1; // 22
+  KUINT32 m_State                 : 1; // 23
+  KUINT32 m_Unused4               : 4; // 24-27
+  KUINT32 m_ExteriorLights        : 1; // 28
+  KUINT32 m_InteriorLights        : 1; // 29
+  KUINT32 m_Unused5               : 1; // 30
+  KUINT32 m_MaskedCloaked         : 1; // 31
 
 public:
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityDamage
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::GetEntityDamage
-    // Description: Entity Damage State
-    // Parameter:   EntityDamage ED
-    //************************************
-    void SetEntityDamage( KDIS::DATA_TYPE::ENUMS::EntityDamage ED );
-    KDIS::DATA_TYPE::ENUMS::EntityDamage GetEntityDamage() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityDamage
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::GetEntityDamage
+  // Description: Entity Damage State
+  // Parameter:   EntityDamage ED
+  //************************************
+  void SetEntityDamage( KDIS::DATA_TYPE::ENUMS::EntityDamage ED );
+  KDIS::DATA_TYPE::ENUMS::EntityDamage GetEntityDamage() const;
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntitySmoke,
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::GetEntitySmoke
-    // Description: Entity Smoke State
-    // Parameter:   EntitySmoke ES
-    //************************************
-    void SetEntitySmoke( KDIS::DATA_TYPE::ENUMS::EntitySmoke ES );
-    KDIS::DATA_TYPE::ENUMS::EntitySmoke GetEntitySmoke() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntitySmokeEmanating,
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntitySmokeEmanating
+  // Description: Entity Smoke Emanating State
+  // Parameter:   KBOOL ESE
+  //************************************
+  void SetEntitySmokeEmanating( KBOOL ESE );
+  KBOOL IsEntitySmokeEmanating() const;
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityFlamingEffect
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityFlaming
-    // Description: Are flames rising from the entity?
-    // Parameter:   KBOOL EFE
-    //************************************
-    void SetEntityFlamingEffect( KBOOL EFE );
-    KBOOL IsEntityFlaming() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityFlamingEffect
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityFlaming
+  // Description: Are flames rising from the entity?
+  // Parameter:   KBOOL EFE
+  //************************************
+  void SetEntityFlamingEffect( KBOOL EFE );
+  KBOOL IsEntityFlaming() const;
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityFrozenStatus
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityFrozen
-    // Description: Is the entity frozen?
-    //              Note: Frozen entities should not be dead-reckoned, they should remain 
-	//              frozen in place. You would likely freeze entites when your application is
-	//              in a paused state.
-    // Parameter:   KBOOL EFS
-    //************************************
-    void SetEntityFrozenStatus( KBOOL EFS );
-    KBOOL IsEntityFrozen() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityFrozenStatus
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityFrozen
+  // Description: Is the entity frozen?
+  //              Note: Frozen entities should not be dead-reckoned, they should remain
+  //              frozen in place. You would likely freeze entites when your application is
+  //              in a paused state.
+  // Parameter:   KBOOL EFS
+  //************************************
+  void SetEntityFrozenStatus( KBOOL EFS );
+  KBOOL IsEntityFrozen() const;
 
-	//************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetInternalHeatStatusOn
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsInternalHeatOn
-    // Description: Is the feature emitting internal heat?
-    // Parameter:   KBOOL IHS
-    //************************************
-    void SetInternalHeatStatusOn( KBOOL IHS );
-    KBOOL IsInternalHeatOn() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetInternalHeatStatusOn
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsInternalHeatOn
+  // Description: Is the feature emitting internal heat?
+  // Parameter:   KBOOL IHS
+  //************************************
+  void SetInternalHeatStatusOn( KBOOL IHS );
+  KBOOL IsInternalHeatOn() const;
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityStateActive
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityStateActive
-    // Description: True = Active, False = Deactivated.
-    // Parameter:   KBOOL ES
-    //************************************
-    void SetEntityStateActive( KBOOL ES );
-    KBOOL IsEntityStateActive() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityStateActive
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityStateActive
+  // Description: True = Active, False = Deactivated.
+  // Parameter:   KBOOL ES
+  //************************************
+  void SetEntityStateActive( KBOOL ES );
+  KBOOL IsEntityStateActive() const;
 
-	//************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityExteriorLights
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityExteriorLightsOn
-    // Description: Are the exterior lights turned on? True = On, False = Off.
-    // Parameter:   KBOOL EL
-    //************************************
-    void SetEntityExteriorLights( KBOOL EL );
-    KBOOL IsEntityExteriorLightsOn() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityExteriorLights
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityExteriorLightsOn
+  // Description: Are the exterior lights turned on? True = On, False = Off.
+  // Parameter:   KBOOL EL
+  //************************************
+  void SetEntityExteriorLights( KBOOL EL );
+  KBOOL IsEntityExteriorLightsOn() const;
 
-	//************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityInteriorLights
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityInteriorLightsOn
-    // Description: Are the vehicles interior lights turned on? True = On, False = Off.
-    // Parameter:   KBOOL IL
-    //************************************
-    void SetEntityInteriorLights( KBOOL IL );
-    KBOOL IsEntityInteriorLightsOn() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntityInteriorLights
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityInteriorLightsOn
+  // Description: Are the vehicles interior lights turned on? True = On, False = Off.
+  // Parameter:   KBOOL IL
+  //************************************
+  void SetEntityInteriorLights( KBOOL IL );
+  KBOOL IsEntityInteriorLightsOn() const;
 
-	//************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntitySurrdenderState
-    //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityMaskedCloaked
-    // Description: Is the entity masked/cloaked or not. 
-	//              True = Masked/Cloaked, False = Not Masked/Cloaked.
-    // Parameter:   KBOOL MC
-    //************************************
-    void SetEntityMaskedCloaked( KBOOL MC );
-    KBOOL IsEntityMaskedCloaked() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::SetEntitySurrdenderState
+  //              KDIS::DATA_TYPE::CulturalFeatureAppearance::IsEntityMaskedCloaked
+  // Description: Is the entity masked/cloaked or not.
+  //              True = Masked/Cloaked, False = Not Masked/Cloaked.
+  // Parameter:   KBOOL MC
+  //************************************
+  void SetEntityMaskedCloaked( KBOOL MC );
+  KBOOL IsEntityMaskedCloaked() const;
 
-    //************************************
-    // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::GetAsString
-    // Description: Returns a string representation
-    //************************************
-    KString GetAsString() const;
+  //************************************
+  // FullName:    KDIS::DATA_TYPE::CulturalFeatureAppearance::GetAsString
+  // Description: Returns a string representation
+  //************************************
+  KString GetAsString() const;
 
-    KBOOL operator == ( const CulturalFeatureAppearance & Value ) const;
-    KBOOL operator != ( const CulturalFeatureAppearance & Value ) const;
+  KBOOL operator == ( const CulturalFeatureAppearance & Value ) const;
+  KBOOL operator != ( const CulturalFeatureAppearance & Value ) const;
 };
 
 } // END namespace DATA_TYPES
