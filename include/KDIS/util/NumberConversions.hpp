71b928df0ee07d98dd27e1c6bf21fed5101171fe
Dave K. Smith <dave.k.smith@gmail.com>
2025-03-12 23:28:48 -0500

    fix: add KDIS_EXPORT to double_to_int32() declaration for Windows linker

--- include/KDIS/util/NumberConversions.hpp
+++ include/KDIS/util/NumberConversions.hpp
@@ -3,12 +3,14 @@
 #include <cstdint>
 #include <stdexcept>
 
+#include "KDIS/KExport.hpp"
+
 namespace KDIS {
 namespace UTIL {
 
 // This function throws on NaN, infinity, overflow, and underflow. It truncates
 // toward zero on a successful return.
-std::int32_t double_to_int32(double value);
+KDIS_EXPORT std::int32_t double_to_int32(double value);
 
 }  // namespace UTIL
 }  // namespace KDIS
